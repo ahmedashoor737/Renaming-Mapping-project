b6705150793cbb15ebb0700cbd6bf2d3ea2f6506
OmarWKH <Omar.w.kh@gmail.com>
2015-11-28 12:07:04 +0300

    reorganizing

---
+++ unnamedrenamer/BibFile.java
@@ -0,0 +1,12 @@
+import java.nio.file.Path;
+
+public class BibFile {
+	String findBibItem(String title,Path bibFilePath){
+		return null;
+		
+	}
+	boolean write(String bibItem,Path bibFilePath){
+		return false;
+		
+	}
+}
---
+++ unnamedrenamer/BibTexFinder.java
@@ -0,0 +1,12 @@
+
+public class BibTexFinder {
+String bibItem;
+
+BibItem findBibItemByTitle(String title, Paper paper) {
+	
+}
+BibItem findBibItemByCitation(String citation, Paper paper) {
+	
+}
+
+}
