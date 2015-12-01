e2c8d594393b077b5b80cb0e267bcd506e444f2b
OmarWKH <Omar.w.kh@gmail.com>
2015-12-01 14:48:19 +0300

    indentation

--- unnamedrenamer/BibTexFinder.java
+++ unnamedrenamer/BibTexFinder.java
@@ -2,29 +2,29 @@
 public class BibTexFinder {
 	private String bibItem;
 	
-String getBibItem(){
-	return bibItem;
-}
-/**
- * this function is given a title then returns a
- *  bibitem from internet
- *  @param title paper's title
- *  @param paper paper to search for
- */
+	String getBibItem(){
+		return bibItem;
+	}
+	/**
+	 * this function is given a title then returns a
+	 *  bibitem from internet
+	 *  @param title paper's title
+	 *  @param paper paper to search for
+	 */
 
-BibItem findBibItemByTitle(String title, Paper paper) {
-	return null;
-	
-}
-/**
- * this function is given a citation then returns a
- *  bibitem from internet
- *  @param citation citation from references
- *  @param paper paper to search for
- */
-BibItem findBibItemByCitation(String citation, Paper paper) {
-	return null;
-	
-}
+	BibItem findBibItemByTitle(String title, Paper paper) {
+		return null;
+		
+	}
+	/**
+	 * this function is given a citation then returns a
+	 *  bibitem from internet
+	 *  @param citation citation from references
+	 *  @param paper paper to search for
+	 */
+	BibItem findBibItemByCitation(String citation, Paper paper) {
+		return null;
+		
+	}
 
 }
