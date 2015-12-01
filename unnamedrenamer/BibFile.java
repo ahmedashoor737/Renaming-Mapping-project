0fd8fcb14bd60183d08d097dd0eb2e7ad4cd7194
OmarWKH <Omar.w.kh@gmail.com>
2015-12-01 13:51:19 +0300

    adding back ahmed's edits

--- unnamedrenamer/BibFile.java
+++ unnamedrenamer/BibFile.java
@@ -1,10 +1,22 @@
 import java.nio.file.Path;
 
 public class BibFile {
+	/**
+	 * 
+	 * @param title title of paper
+	 * @param bibFilePath path to the bib file
+	 * @return bib item for this paper
+	 */
 	String findBibItem(String title,Path bibFilePath){
 		return null;
 		
 	}
+	/**
+	 * 
+	 * @param bibItem bib item of paper
+	 * @param bibFilePath path to bib file
+	 * @return bib item 
+	 */
 	boolean write(String bibItem,Path bibFilePath){
 		return false;
 		
