ea7dfcb246a3d91b4490fc9d09ddd444b2476cc4
m-sb3an <m.c.gt@live.com>
2015-12-02 00:01:06 +0300

    Create PDFReader.java

---
+++ unnamedrenamer/PDFReader.java
@@ -0,0 +1,32 @@
+
+import java.util.List;
+
+
+
+
+public class PDFReader 
+{
+	List<String> fileAttributes ;
+	List<String> refernces ;
+	
+	
+	
+	
+	
+	public String findTitle ( String fileName)
+	{
+		return null ; 
+	}
+	
+	public String getFileAttributes (String fileName)
+	{
+		return null ; 
+	}
+	
+	
+	public List<Paper> extractReferences (String fileName)
+	{
+		return null ;
+	}
+
+}
