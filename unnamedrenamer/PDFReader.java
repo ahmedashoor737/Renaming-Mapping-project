1f7bf9db70f71d1c94ca6c50c6ea31c11161fdd5
ahmedashoor737 <ahmed.m.ashoor@gmail.com>
2015-12-08 00:53:46 +0300

    title done :(

--- unnamedrenamer/PDFReader.java
+++ unnamedrenamer/PDFReader.java
@@ -1,5 +1,6 @@
 
 import java.util.List;
+import java.util.StringTokenizer;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,33 +11,62 @@ import java.nio.file.attribute.BasicFileAttributes;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.util.TextPosition;
 
 
 
-public class PDFReader 
+public class PDFReader extends PDFTextStripper
 {
+	public PDFReader() throws IOException {
+		super();
+		
+	}
+
+
 	List<String> fileAttributes ;
 	List<String> refernces ;
 	
 	PDDocument paper;
-	PDFTextStripper strip = new PDFTextStripper();
+	PDFTextStripper strip ;
 	PDDocumentInformation info ;
 	
 	
 	
-	public String findTitle ( File fileLocation ) //changed string filename to File filelocation
+	public String findTitle ( File fileLocation ) throws IOException //changed string filename to File filelocation
 	{
+		
+		strip.setSortByPosition(true);
+		strip.setStartPage(1);
+		strip.setEndPage(1);
+		
 		paper = PDDocument.load(fileLocation);
 		info = 	paper.getDocumentInformation();
 
-		if (info.getTitle() == null) //still working on algorithm to handle not founding title
-		else	
-		return info.getTitle().trim(); //function provided by api to get title
+		String titleWithWhiteSpaces = strip.getText(paper);
+		StringBuilder title = null;
+		StringTokenizer tk =new StringTokenizer(titleWithWhiteSpaces);
+		while(tk.hasMoreTokens() ){
+			   
+			title.append(tk.nextToken() + " ");
+			   }
+		
+		if (title.toString().isEmpty() || title.toString() == null)
+			title.append(info.getTitle());
+		
+		
+		
+		
+		return title.toString();
+		
+		
+		
+			
+		
 	}
 	
-	public String getFileAttributes (File fileLocation)//changed string filename to File filelocation
+	public String getFileAttributes (File fileLocation) throws IOException//changed string filename to File 
 	{
-		BasicFileAttributes attr = Files.readAttributes(fileLocation.getPath(), BasicFileAttributes.class);
+		BasicFileAttributes attr = Files.readAttributes(fileLocation.toPath(), BasicFileAttributes.class);
 		
 		paper = PDDocument.load(fileLocation);
 		info = 	paper.getDocumentInformation();
@@ -47,7 +77,34 @@ public class PDFReader
 	
 	public List<Paper> extractReferences (File fileLocation)//changed string filename to File filelocation
 	{
-		return null ;//still working on a way to read pdf file without changing format and a way to strip the region of references
+		return null ;//leaving this to cermine
 	}
+	
+	
+	
+	
+	
+	
+	
+	//override the text extracting method of PDFTextStripper 
+	 protected void writeString(String text, List<TextPosition> textPositions) throws IOException
+	    {
+	        StringBuilder builder = new StringBuilder();
+
+	        for (TextPosition position : textPositions)
+	        {
+	            
+	             
+	            if (position.getFontSizeInPt() >=13 &&position.getY()>0 && position.getY()<500)
+	            {
+	                builder.append(position.getCharacter());
+	            }
+	             
+	            
+	        }
+	        
+	       writeString(builder.toString());
+	        
+	    }
 
 }
